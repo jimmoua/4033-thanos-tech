186e04d7be15b7fb14845d2db35b70ad9277802c
Jim Moua <jimmoua24@gmail.com>
2020-03-13 21:34:49 -0500

    Changed 4 space to 2 space. Set static folder.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Requires
 const express = require('express');
 const expressSession = require('express-session');
+const path = require('path');
 
 // Create server called app
 const app = express();
@@ -9,6 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Set static folder for public files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set templating engine to EJS
 app.set('view engine', 'ejs');
 
@@ -16,7 +20,7 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+  res.render('index.ejs');
 });
 
 // Listen on port
