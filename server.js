59d9e53a077fbd0e7d62203c821bafd149813c87
Jim Moua <jimmoua24@gmail.com>
2020-03-15 01:50:07 -0500

    Added routes for home routing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Requires
 const express = require('express');
-const expressSession = require('express-session');
+const session = require('express-session');
 const path = require('path');
 
 // Create server called app
@@ -9,6 +9,9 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'foobar'
+}))
 
 // Set static folder for public files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,9 +22,7 @@ app.set('view engine', 'ejs');
 // Define our port
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+app.get('/', require('./routes/home_views'));
 
 // Listen on port
 app.listen(port, () => console.log(`Listening on port ${port}.`));
