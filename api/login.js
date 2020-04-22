387f84b46eff3d9b43e291a2a7ed2f24a01ce60a
Jim Moua <jimmoua24@gmail.com>
2020-04-22 01:09:44 -0500

    Added auth error if email not found in DB.

--- api/login.js
+++ api/login.js
@@ -10,18 +10,23 @@ router.post('/api/loginStudent', (req, res) => {
   const email = req.body.email;
   db.query(`SELECT * FROM STUDENT WHERE EMAIL = '${email}';`, (err, results) => {
     if(err) throw err;
-    const hash = results[0].PASSWORD;
-    bcrypt.compare(pass, hash, (err, result) => {
-      // If result == true, the provided password was correct
-      if(result) {
-        req.session.loggedIn = true;
-        req.session.loggedInType = ACCOUNT.STUDENT;
-        res.send(`Hello, ${results[0].FNAME} ${results[0].LNAME}.`);
-      }
-      else {
-        res.send(`Auth error!`)
-      }
-    })
+    if(results.length < 1) {
+      res.send(`Auth error!`);
+    }
+    else {
+      const hash = results[0].PASSWORD;
+      bcrypt.compare(pass, hash, (err, result) => {
+        // If result == true, the provided password was correct
+        if(result) {
+          req.session.loggedIn = true;
+          req.session.loggedInType = ACCOUNT.STUDENT;
+          res.send(`Hello, ${results[0].FNAME} ${results[0].LNAME}.`);
+        }
+        else {
+          res.send(`Auth error!`)
+        }
+      })
+    }
   });
 })
 
