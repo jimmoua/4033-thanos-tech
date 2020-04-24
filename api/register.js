0429d8bba7c6f704c47986953d614a61e4ecac57
Jim Moua <jimmoua24@gmail.com>
2020-04-24 01:29:44 -0500

    Integrated Fahim code and fixed undefined password bug.

--- api/register.js
+++ api/register.js
@@ -45,6 +45,34 @@ router.post('/:type', (req, res) => {
       break;
     }
     case ACCOUNT.TUTOR: {
+      bcrypt.genSalt(saltRounds, (err, salt) => {
+        bcrypt.hash(pass1, salt, (err, hash) => {
+          const accID = uuid(); 
+          db.query(`SELECT * FROM TUTOR WHERE ACC_NO = '${accID}'`, (err, results) => {
+            if (err) throw err; 
+            if (results.length > 0) {
+              res.send(`A user with the email: '${email}' already exists`); 
+            }
+            else {
+              db.query(`INSERT INTO TUTOR (ACC_NO, FNAME, LNAME, EMAIL, PASSWORD) VALUES ('${accID}', '${fname}', '${lname}', '${email}', '${hash}');`, (err, results) => {
+                if (err) throw err; 
+                console.log(results); 
+              })
+              db.query(`SELECT * FROM TUTOR WHERE ACC_NO = '${accID}'`, (err, results) => {
+                if (err) throw err; 
+                console.log(results); 
+              })
+              res.send(`You've registered with the account : "${email}"
+              Redirecting to the login page in 3 seconds
+              <script>
+              setTimeout(function() {
+                window.location.href='/login'
+              }, 3000);
+              </script>`)
+            }
+          })
+        })
+      })
       break;
     }
     case ACCOUNT.PARENT: {
@@ -76,53 +104,6 @@ router.post('/:type', (req, res) => {
       break;
     }
   }
-})
-
-// POST method for /api/registerTutor
-router.post("/api/registerTutor", (req, res) => {
-  const firstName = req.body.firstName; 
-  const lastName = req.body.lastName; 
-  const email = req.body.email; 
-  const tutorPass1 = req.body.tutorPass1; 
-  const tutorPass2 = req.body.tutorPass2; 
-
-  if(tutorPass1 !== tutorPass2) {
-    res.status(400).send('Bad Request');
-  }
-  else {
-    const saltRounds = 10; 
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(tutorPass1, salt, (err, hash) => {
-        const accID = uuid(); 
-        db.query(`SELECT * FROM TUTOR WHERE ACC_NO = '${accID}'`, (err, results) => {
-          if (err) throw err; 
-          if (results.length > 0) {
-            res.send(`A user with the email: '${email}' already exists`); 
-            exists = true; 
-          }
-          else {
-            db.query(`INSERT INTO TUTOR (ACC_NO, FNAME, LNAME, EMAIL, PASSWORD) VALUES ('${accID}', '${firstName}', '${lastName}', '${email}', '${hash}');`, (err, results) => {
-              if (err) throw err; 
-              console.log(results); 
-            })
-            db.query(`SELECT * FROM TUTOR WHERE ACC_NO = '${accID}'`, (err, results) => {
-              if (err) throw err; 
-              console.log(results); 
-            })
-            res.send(`You've registered with the account : "${email}"
-            Redirecting to the login page in 3 seconds
-            <script>
-            setTimeout(function() {
-              window.location.href='/login'
-            }, 3000);
-            </script>`)
-          }
-        })
-      })
-    })
-  }
-  // To do: send this data to database
-  res.send(`${firstName} ${lastName} registered as a tutor with the email '${email}'`); 
 })
 
 module.exports = router;
