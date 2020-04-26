7d9dcb0327fabf38084dfddcfba9fda6550136b7
Shahriar Fahim <shahriarfahim8700@gmail.com>
2020-04-26 18:11:11 -0500

    Updated post methods with session validation

--- api/users/student.js
+++ api/users/student.js
@@ -4,51 +4,61 @@ const ACCOUNT = require('../../misc/accountTypes');
 const db = require('../../db_files/db');
 
 router.post('/setParentAccount', (req, res) => {
-  if(req.session.user.type != ACCOUNT.STUDENT) {
-    res.status(400).send('Bad request.');
+  if (!req.session.user) {
+    res.redirect('/'); 
   }
   else {
-    const pemail = req.body.pemail;
-    console.log('the email is ', pemail)
-    db.query(`select ACC_NO from PARENT where EMAIL = '${pemail}';`, (err, p_results) => {
-      if(p_results.length == 0) {
-        res.send(`The parent email doesn't exist`);
-      }
-      else {
-        db.query(`update STUDENT set PARENT_ACC_NO = '${p_results[0].ACC_NO}' where ACC_NO = '${req.session.user.acc_no}'`, (err, results) => {
-          if(err) {
-            res.send(`${err}`);
-            throw err;
-          }
-          else {
-            res.redirect('/');
-          }
-        })
-      }
-    })
+    if(req.session.user.type != ACCOUNT.STUDENT) {
+      res.status(400).send('Bad request.');
+    }
+    else {
+      const pemail = req.body.pemail;
+      console.log('the email is ', pemail)
+      db.query(`select ACC_NO from PARENT where EMAIL = '${pemail}';`, (err, p_results) => {
+        if(p_results.length == 0) {
+          res.send(`The parent email doesn't exist`);
+        }
+        else {
+          db.query(`update STUDENT set PARENT_ACC_NO = '${p_results[0].ACC_NO}' where ACC_NO = '${req.session.user.acc_no}'`, (err, results) => {
+            if(err) {
+              res.send(`${err}`);
+              throw err;
+            }
+            else {
+              res.redirect('/');
+            }
+          })
+        }
+      })
+    }
   }
 });
 
 router.post('/updateProfile', (req, res) => {
-  if (res.session.user.type != ACCOUNT.STUDENT) {
+  if (!res.session.user) {
     res.redirect('/'); 
   }
   else {
-    let data = {
-      bio: req.body.bio,
-      gender: req.body.gender,
-      acc_no: req.session.user.acc_no
+    if (res.session.user.type != ACCOUNT.STUDENT) {
+      res.redirect('/'); 
     }
-    if(data.gender == 'none') data.gender = null;
     else {
-      data.gender = data.gender == 'male' ? 'M' : 'F';
-    }
-    db.query(`update STUDENT set BIO = ?, GENDER = ? where ACC_NO = ?`, [data.bio, data.gender, data.acc_no], (err, results) => {
-      if(err) {
-        throw err;
+      let data = {
+        bio: req.body.bio,
+        gender: req.body.gender,
+        acc_no: req.session.user.acc_no
       }
-      res.redirect('/');
-    })
+      if(data.gender == 'none') data.gender = null;
+      else {
+        data.gender = data.gender == 'male' ? 'M' : 'F';
+      }
+      db.query(`update STUDENT set BIO = ?, GENDER = ? where ACC_NO = ?`, [data.bio, data.gender, data.acc_no], (err, results) => {
+        if(err) {
+          throw err;
+        }
+        res.redirect('/');
+      })
+    }
   }
 })
 
