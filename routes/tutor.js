a2378114587ee775d15ba9d7e71d9a01d4314558
Jim Moua <jimmoua24@gmail.com>
2020-04-26 23:12:42 -0500

    Added tutor routes and EJS.
    Co-authored-by: shahriarfahim870 <shahriarfahim8700@gmail.com>

--- routes/tutor.js
+++ routes/tutor.js
@@ -1,5 +1,57 @@
 const express = require('express')
 const router = express.Router();
 const db = require('../db_files/db');
+const ACCOUNT = require('../misc/accountTypes');
+
+router.get('/editprofile', (req, res) => {
+  if (!req.session.user || req.session.user.type != ACCOUNT.TUTOR) {
+    res. redirect('/'); 
+  }
+  else {
+    const data = {
+      table: 'TUTOR',
+      acc_no: req.session.user.acc_no
+    }
+    db.query(`select FNAME, LNAME, EMAIL, GENDER, BIO from ?? where ACC_NO = ?`, [data.table, data.acc_no], (err, rows) => {
+      if(err) throw err;
+      else {
+        res.render('tutor/editprofile', {
+          fname : rows[0].FNAME,
+          lname : rows[0].LNAME,
+          email : rows[0].EMAIL,
+          gender: rows[0].GENDER,
+          bio   : rows[0].BIO
+        })
+      }
+    })
+  }
+})
+
+router.get('/viewscheduledappointments', (req, res) => {
+  if (!req.session.user || req.session.user.type != ACCOUNT.TUTOR) {
+    res.redirect('/'); 
+  }
+  else {
+    res.render('tutor/scheduledappointment');
+  }
+})
+
+router.get('/manageincome', (req, res) => {
+  if(!req.session.user || req.session.user.type != ACCOUNT.TUTOR) {
+    res.redirect('/'); 
+  }
+  else {
+    res.render('tutor/manageincome');
+  }
+})
+
+router.get('/viewappointmenthistory', (req, res) => {
+  if(!req.session.user || req.session.user.type != ACCOUNT.TUTOR) {
+    res.redirect('/');
+  }
+  else {
+    res.render('tutor/appointmenthistory')
+  }
+})
 
 module.exports = router;
