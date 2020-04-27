6345a959165012bd93d1b0f689347486d9f5893a
Jim Moua <jimmoua24@gmail.com>
2020-04-27 14:52:50 -0500

    typos fix

--- api/users/tutor.js
+++ api/users/tutor.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ACCOUNT = require('../../misc/accountTypes');
 const db = require('../../db_files/db');
+const {v4: uuid} = require('uuid');
 
 
 // "/tutor/updateprofile"
@@ -13,7 +14,7 @@ router.post('/updateprofile', (req, res) => {
       fname: req.body.fname,
       lname: req.body.lname,
       gender: req.body.gender,
-      bio: req.body.bio ? req.body.io : null,
+      bio: req.body.bio ? req.body.bio : null,
       email: req.body.email,  
       course: req.body.courseAdd ? req.body.courseAdd : null
     }
@@ -23,8 +24,8 @@ router.post('/updateprofile', (req, res) => {
     }
     // Run the query and update
     db.query(`UPDATE TUTOR SET FNAME = ?, LNAME = ?, GENDER = ?, BIO = ?, EMAIL = ? WHERE ACC_NO = ?`, [data.fname, data.lname, data.gender, data.bio, data.email, req.session.user.acc_no], (err, results) => {
-      if(data.courses) {
-        db.query(`INSERT INTO COURSES VALUES (?, ?)`, [req.session.user.acc_no, data.course], (err, courseResults) => {
+      if(data.course) {
+        db.query(`INSERT INTO COURSES VALUES (?, ?, ?)`, [req.session.user.acc_no, uuid(), data.course], (err, courseResults) => {
           if (err) throw err;
           res.redirect('/');
         })
