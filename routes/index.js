dad192ac4a3f9cf1d508da7c1d564cbe4aeb42a3
Jim Moua <jimmoua24@gmail.com>
2020-04-26 21:39:00 -0500

    Added routes for tutor and parent.
    Co-authored-by: shahriarfahim870 <shahriarfahim8700@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router
   .use('/', require('./home_views'))
   .use('/student/', require('./student'))
+  .use('/tutor/', require('./tutor'))
+  .use('/parent/', require('./parent')); 
 
 module.exports = router;
