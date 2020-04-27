a608321ab6dd0c7c8e4838e3164d3471485ba3d6
Jim Moua <jimmoua24@gmail.com>
2020-04-26 23:42:53 -0500

    edit profile for tutor
    Co-authored-by: shahriarfahim870 <shahriarfahim8700@gmail.com>

--- api/users/tutor.js
+++ api/users/tutor.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const ACCOUNT = require('../../misc/accountTypes');
+const db = require('../../db_files/db');
+
+
+// "/tutor/updateprofile"
+router.post('/updateprofile', (req, res) => {
+  if(!req.session.user || req.session.user.type !== ACCOUNT.TUTOR) {
+    res.redirect('/');
+  } else {
+    let data = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      gender: req.body.gender,
+      bio: req.body.bio,
+      email: req.body.email  
+    }
+    if(data.gender == 'none') data.gender = null;
+    else {
+      data.gender = data.gender == 'male' ? 'M' : 'F';
+    }
+    // Run the query and update
+    db.query(`UPDATE TUTOR SET FNAME = ?, LNAME = ?, GENDER = ?, BIO = ?, EMAIL = ? WHERE ACC_NO = ?`, [data.fname, data.lname, data.gender, data.bio, data.email, req.session.user.acc_no], (err, results) => {
+      if (err) throw err; 
+      res.redirect('/');
+    });
+  }
+});
 
 module.exports = router;
