a14d134c897b279aaf579b5b74eef3c26da6cb44
Jim Moua <jimmoua24@gmail.com>
2020-04-28 22:54:19 -0500

    Implement POST request for canceling child appointment.

--- api/users/parent.js
+++ api/users/parent.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db_files/db');
+const ACCOUNT = require('../../misc/accountTypes');
+
+router.post('/cancelappointment', (req, res) => {
+  if(!req.session.user || req.session.user.type !== ACCOUNT.PARENT) {
+    res.redirect('/');
+  }
+  if(req.query.aptid) {
+    const aptid = req.query.aptid;
+    db.query(`delete from APPOINTMENTS where APPOINTMENT_ID = ?`, [aptid], (err) => {
+      if(err) {
+        res.json(err);
+        return;
+      }
+      res.redirect('/parent/scheduledappointments');
+    })
+  }
+})
 
 module.exports = router;
