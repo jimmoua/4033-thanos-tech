4d829c97a283e0a5742e8ba6013afa90ddc1d8ea
Jim Moua <jimmoua24@gmail.com>
2020-04-29 18:21:54 -0500

    added PAID_BY column

--- db_files/transactions.sql
+++ db_files/transactions.sql
@@ -5,6 +5,7 @@ CREATE TABLE `TRANSACTIONS` (
   `STUDENT_ID`      varchar(128)    NOT NULL,
   `STATUS`          char(10)        NOT NULL,
   `AMOUNT`          decimal(10,2)   NOT NULL,
+  `PAID_BY`         varchar(128)    NULL,
   PRIMARY KEY                       (`TRANSACTION_ID`),
   KEY             `FKTUTOR`         (`TUTOR_ID`),
   KEY             `FKSTUDENT`       (`STUDENT_ID`),
