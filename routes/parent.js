576a3dd176c331ba2f675cc78d1bd76243e370b6
Jim Moua <jimmoua24@gmail.com>
2020-04-29 23:17:25 -0500

    Added 404 routing for view queries.

--- routes/parent.js
+++ routes/parent.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../db_files/db');
 const ACCOUNT = require('../misc/accountTypes');
 const {v4: uuid} = require('uuid');
+const path = require('path');
 
 router.get('/scheduledappointments', (req, res) => {
   // * If no user session defined or trying to spoof
@@ -20,6 +21,10 @@ router.get('/scheduledappointments', (req, res) => {
         res.json(err);
         return;
       }
+      if(results.length == 0) {
+        res.status(404).sendFile(path.resolve('public/html/404.html'));
+        return;
+      }
       res.render('parent/scheduleview.ejs', {
         apt: results[0]
       })
@@ -29,6 +34,10 @@ router.get('/scheduledappointments', (req, res) => {
   else {
     const qstring = `select	APPOINTMENTS.APPOINTMENT_ID, APPOINTMENTS.PLACE, STUDENT.ACC_NO as SACCNO, STUDENT.FNAME AS SFNAME, STUDENT.LNAME AS SLNAME, STUDENT.EMAIL AS SEMAIL, TUTOR.ACC_NO AS TACCNO, TUTOR.FNAME AS TFNAME, TUTOR.LNAME AS TLNAME, TUTOR.EMAIL AS TEMAIL, APPOINTMENTS.APPOINTMENT_DATE AS DATE, APPOINTMENTS.APPOINTMENT_TIME AS TIME, APPOINTMENTS.STATUS, COURSES.COURSE_NAME from STUDENT right outer join APPOINTMENTS on STUDENT.ACC_NO in(APPOINTMENTS.STUDENT_ID) right outer join TUTOR ON APPOINTMENTS.TUTOR_ID IN(TUTOR.ACC_NO) right outer join COURSES ON COURSES.COURSE_ID IN(APPOINTMENTS.COURSE) where PARENT_ACC_NO = ?`
     db.query(qstring, [req.session.user.acc_no], (err, results) => {
+      if(results.length == 0) {
+        res.sendFile(path.resolve('public/html/404.html'));
+        return;
+      }
       if(err) {
         res.json(err);
         return;
