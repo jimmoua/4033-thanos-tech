dcb3274b35b381235cffa81ed07a3cc2652904f9
Jim Moua <jimmoua24@gmail.com>
2020-04-29 23:31:41 -0500

    Removed 404 - didn't mean to add it there.

--- routes/parent.js
+++ routes/parent.js
@@ -34,10 +34,6 @@ router.get('/scheduledappointments', (req, res) => {
   else {
     const qstring = `select	APPOINTMENTS.APPOINTMENT_ID, APPOINTMENTS.PLACE, STUDENT.ACC_NO as SACCNO, STUDENT.FNAME AS SFNAME, STUDENT.LNAME AS SLNAME, STUDENT.EMAIL AS SEMAIL, TUTOR.ACC_NO AS TACCNO, TUTOR.FNAME AS TFNAME, TUTOR.LNAME AS TLNAME, TUTOR.EMAIL AS TEMAIL, APPOINTMENTS.APPOINTMENT_DATE AS DATE, APPOINTMENTS.APPOINTMENT_TIME AS TIME, APPOINTMENTS.STATUS, COURSES.COURSE_NAME from STUDENT right outer join APPOINTMENTS on STUDENT.ACC_NO in(APPOINTMENTS.STUDENT_ID) right outer join TUTOR ON APPOINTMENTS.TUTOR_ID IN(TUTOR.ACC_NO) right outer join COURSES ON COURSES.COURSE_ID IN(APPOINTMENTS.COURSE) where PARENT_ACC_NO = ?`
     db.query(qstring, [req.session.user.acc_no], (err, results) => {
-      if(results.length == 0) {
-        res.sendFile(path.resolve('public/html/404.html'));
-        return;
-      }
       if(err) {
         res.json(err);
         return;
