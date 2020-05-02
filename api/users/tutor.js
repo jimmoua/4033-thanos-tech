2e32f4f22144df1d125eeb5728f69d7e5d1f4833
Jim Moua <jimmoua24@gmail.com>
2020-05-02 04:53:23 -0500

    removed amount from the frontend and backend for tutor.

--- api/users/tutor.js
+++ api/users/tutor.js
@@ -52,13 +52,6 @@ router.post('/accept', (req, res) => {
   }
   if(req.query.aptid) {
     // If there is a query to accept the appointment given appointment ID (aptid)
-    const amt = req.body.amount;
-    // Check for amount. We have a check implemented where it has to be [0, inf], but client can always
-    // modify the HTML attributes. In that case, send a 400.
-    if(!amt || amt < 0 || isNaN(amt)) {
-      res.status(400).sendFile(path.resolve('public/html/400.html'))
-      return;
-    }
 
     db.query(`select * from APPOINTMENTS where APPOINTMENT_ID = ?`, [req.query.aptid], (err, apt_details) => {
       if(err) {
@@ -71,7 +64,7 @@ router.post('/accept', (req, res) => {
       }
 
       // Update the appointments and set the status to true
-      db.query(`UPDATE APPOINTMENTS SET STATUS = 'ACCEPTED' WHERE APPOINTMENT_ID = ? `, [req.query.aptid], (err, results) => {
+      db.query(`UPDATE APPOINTMENTS SET STATUS = 'ACCEPTED' WHERE APPOINTMENT_ID = ? `, [req.query.aptid], (err) => {
         if(err) {
           res.status(500).json(err);
         }
