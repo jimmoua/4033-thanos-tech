ea82d0cf0d8bb27827c602328d25cc82ecb9f0a6
Jim Moua <jimmoua24@gmail.com>
2020-05-04 05:36:04 -0500

    session store creds change

--- db_files/sessionStore.js
+++ db_files/sessionStore.js
@@ -3,7 +3,7 @@ const MySQLStore = require('express-mysql-session')(session);
 const store = new MySQLStore({
     host: '***REMOVED***.cxcs9abwkocb.us-east-2.rds.amazonaws.com',
     user: 'admin',
-    password: '***REMOVED***tech',
+    password: '***REMOVED***white',
     database: 'innodb',
     clearExpired: true,
     checkExpirationInterval: 1000 * 900
