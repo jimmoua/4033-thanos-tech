7f1e0f32f51c65045a97042bd0c3ad9512536cad
Jim Moua <jimmoua24@gmail.com>
2020-05-04 05:28:47 -0500

    db password change - final commit

--- db_files/db.js
+++ db_files/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: '***REMOVED***.cxcs9abwkocb.us-east-2.rds.amazonaws.com',
   user: 'admin',
-  password: '***REMOVED***tech',
+  password: '***REMOVED***white',
   database: 'innodb'
 })
 
