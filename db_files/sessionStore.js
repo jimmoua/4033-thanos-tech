a39206d2577ac7d842951515973c087809362cb3
Jim Moua <jimmoua24@gmail.com>
2021-06-13 02:48:31 -0500

    Fixed MySql connection and env vars file. (#8)

--- db_files/sessionStore.js
+++ db_files/sessionStore.js
@@ -4,7 +4,8 @@ const store = new MySQLStore({
     host: process.env.TWT_RDS_HOST,
     user: process.env.TWT_RDS_USER,
     password: process.env.TWT_RDS_PASSWORD,
-    database: process.env.TWT_RDS_DB,
+    database: process.env.TWT_RDS_DATABASE,
+    port: process.env.TWT_RDS_PORT,
     clearExpired: true,
     checkExpirationInterval: 1000 * 900
 });
